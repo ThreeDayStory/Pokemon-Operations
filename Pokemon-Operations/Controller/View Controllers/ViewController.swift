0d5fd382e3017d2862379674149508c99bd5ba97
Chad Rutherford <chad_rutherford@outlook.com>
2020-03-24 11:24:23 -0400

    Reset project to starter state

---
+++ Pokemon-Operations/Controller/View Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Pokemon-Operations
+//
+//  Created by Chad Rutherford on 3/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+    }
+}
