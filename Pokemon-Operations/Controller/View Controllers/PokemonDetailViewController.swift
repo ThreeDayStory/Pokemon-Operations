e4a4ac5ebeb870a5d660b1fd864f06c4cc1f3db4
Chad Rutherford <chad_rutherford@outlook.com>
2020-03-24 00:49:14 -0400

    Finished Project

--- Pokemon-Operations/Controller/View Controllers/PokemonDetailViewController.swift
+++ Pokemon-Operations/Controller/View Controllers/PokemonDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PokemonDetailViewController: UIViewController {
-
+	
 	@IBOutlet weak var pokemonImageView: UIImageView!
 	@IBOutlet weak var heightLabel: UILabel!
 	@IBOutlet weak var weightLabel: UILabel!
@@ -21,10 +21,11 @@ class PokemonDetailViewController: UIViewController {
 			fetchDetails()
 		}
 	}
+	var pokemonQueue = OperationQueue()
 	
 	override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+		super.viewDidLoad()
+	}
 	
 	private func fetchDetails() {
 		guard let pokemon = pokemonResult, let apiController = apiController else { return }
@@ -39,19 +40,36 @@ class PokemonDetailViewController: UIViewController {
 	}
 	
 	private func updateViews(with pokemon: Pokemon) {
+		// Version 1 - Without NSOperation and Queues
+		//		guard let apiController = apiController else { return }
+		//		heightLabel.text = "\(pokemon.height)"
+		//		weightLabel.text = "\(pokemon.weight)"
+		//		abilityLabel.text = pokemon.abilities
+		//		typeLabel.text = pokemon.types
+		//		// TODO: -  fetch image and display
+		//		apiController.fetchImage(for: pokemon.imageURL) { result in
+		//			switch result {
+		//			case .success(let image):
+		//				self.pokemonImageView.image = image
+		//			case .failure(let error):
+		//				print(error)
+		//			}
+		//		}
+		
+		// Version 2 - Using Operations
 		guard let apiController = apiController else { return }
-		heightLabel.text = "\(pokemon.height)"
-		weightLabel.text = "\(pokemon.weight)"
-		abilityLabel.text = pokemon.abilities
-		typeLabel.text = pokemon.types
-		// TODO: -  fetch image and display
-		apiController.fetchImage(for: pokemon.imageURL) { result in
-			switch result {
-			case .success(let image):
-				self.pokemonImageView.image = image
-			case .failure(let error):
-				print(error)
-			}
+		let imageFetchOperation = ImageFetchOperation(apiController: apiController, url: pokemon.imageURL)
+		let completionOperation = BlockOperation {
+			guard let image = imageFetchOperation.image else { return }
+			self.pokemonImageView.image = image
+			self.heightLabel.text = "\(pokemon.height)"
+			self.weightLabel.text = "\(pokemon.weight)"
+			self.abilityLabel.text = pokemon.abilities
+			self.typeLabel.text = pokemon.types
 		}
+		
+		completionOperation.addDependency(imageFetchOperation)
+		pokemonQueue.addOperation(imageFetchOperation)
+		OperationQueue.main.addOperation(completionOperation)
 	}
 }
